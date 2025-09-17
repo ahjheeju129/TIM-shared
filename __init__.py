ee101895a2ef4cdf9f6687585efaefd2f05f7788
ADMIN\사용자 <heeju1112@naver.com>
2025-09-17 17:46:03 +0900

    package 설명 파일(pyproject.toml, setup.py) 추가, __init__.py 수정

--- __init__.py
+++ __init__.py
@@ -1,1 +1,27 @@
-# Shared utilities and common components for all services
+# currency-shared/__init__.py
+# 1. 다른 모듈들에서 함수/클래스 가져오기
+from .config import init_config, get_config
+from .database import init_database, get_db_manager
+from .logging import get_logger, set_correlation_id
+from .models import ExchangeRate, CollectionResult
+from .utils import DateTimeUtils, SecurityUtils
+from .exceptions import DatabaseError, DataProcessingError
+
+# 2. 패키지 버전 정보
+__version__ = "0.1.0"
+
+# 3. 외부에서 import할 수 있는 것들의 목록
+__all__ = [
+    "init_config",
+    "get_config", 
+    "init_database",
+    "get_db_manager",
+    "get_logger",
+    "set_correlation_id",
+    "ExchangeRate",
+    "CollectionResult",
+    "DateTimeUtils",
+    "SecurityUtils",
+    "DatabaseError",
+    "DataProcessingError",
+]
